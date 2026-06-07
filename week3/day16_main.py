45222228e11c7e41bbb8332c607f65f9cc2b7ff7
Ryuichi Hashimoto <fuzh7270@gmail.com>
2026-06-07 12:04:24 +0200

    week3/day16_main.py

---
+++ week3/day16_main.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def introduce(self):
+        print("My name is ", self.name)
+person1 = Person("Ryuichi", 30)
+
+person1 = Person("Ryuichi", 30)
+
+person1.introduce()
+
+
