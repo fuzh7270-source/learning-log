11bfd690495de5e9c90884db294e313dd44b4ad4
Ryuichi Hashimoto <fuzh7270@gmail.com>
2026-07-04 18:17:05 +0200

    week3/day21_main.py

---
+++ week3/day21_main.py
@@ -0,0 +1,8 @@
+from funktions2 import add, greet, maltiply
+
+greet("Ryuichi")
+
+result = add(3,5)
+print(result)
+
+print(maltiply(4,5))
---
+++ week3/funktions2.py
@@ -0,0 +1,8 @@
+def greet(name):
+    print("Hello", name)
+
+def add(a, b):
+    return a + b
+
+def maltiply(a, b):
+    return a * b
