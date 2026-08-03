d9025dc370455428774a71c72b18148107fd1599
Ryuichi Hashimoto <fuzh7270@gmail.com>
2026-08-03 12:17:51 +0200

    week4/day22_main.py

---
+++ week4/day22_main.py
@@ -0,0 +1,15 @@
+def profile(name,age):
+    print("Name:", name)
+    print("Age:", age)
+
+profile("Ryuichi", 30)
+
+def multiply(x, y):
+    return x*y
+
+print(multiply(5, 6))
+
+def bmi(weight, height):
+    return weight / (height ** 2)
+
+print(bmi(70, 1.81))
